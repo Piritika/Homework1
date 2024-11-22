5b002fdf35270f00fdf7cea1693964edd8c2ca96
Piritika <tearose863@gmail.com>
2024-11-22 21:41:22 +0300

    first commit

---
+++ src/FourNumbers.java
@@ -0,0 +1,12 @@
+public class FourNumbers {
+    public static void main(String[] args) {
+        int one = 11;
+        int two = 22;
+        int three = 33;
+        int four = 44;
+
+        String space = "   ";
+
+        System.out.println(one + space + two + space + three + space + four);
+    }
+}
